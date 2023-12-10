59610e3cb07befd6da58516c426783f12725de42
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-12-09 22:34:46 -0800

    This seems to make things build

--- boost.akibld.bat
+++ boost.akibld.bat
@@ -1,11 +1,15 @@
-@echo off
+:: @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 call "%AKIBLD%\build-event-env.bat" %~f0 %* 
 
 :: boost.vcxproj pre-build 
 
 if [%RunMode%] == [clean] (
-	rmdir /s /q %IntDir%
+	echo ******** CLEANING BOOST LIBRARY | tee -a "%AKILOG%"
+	pushd "%ProjectDir%"
+	git clean -dxf -e .build | tee -a "%AKILOG%"
+	git submodule update --init --recursive | tee -a "%AKILOG%"
+	popd
 )
 
 if [%RunMode%] == [pre] (
@@ -14,11 +18,12 @@ if [%RunMode%] == [pre] (
 	pushd "%ProjectDir%"
 
 	if not exist b2.exe (
-		echo ******** BUILDING B2 TOOL
-		bootstrap
+		echo ******** BUILDING B2 TOOL | tee -a "%AKILOG%"
+		bootstrap | tee -a "%AKILOG%"
+		echo. | tee -a "%AKILOG%"
 	)
 
-	echo ******** BUILDING BOOST LIBRARY
+	echo ******** BUILDING BOOST LIBRARY | tee -a "%AKILOG%"
 
 	set Bitness=64
 	if /i [%Platform%] == ["Win32"] set Bitness=32
@@ -30,18 +35,20 @@ if [%RunMode%] == [pre] (
 	if /i [%Configuration%] == [Debug] set Variant=debug
 	if /i [%Configuration%] == [Release] set Variant=release
 
-	echo ====== variant=!Variant! address-model=!Bitness! ======
+	echo ====== variant=!Variant! address-model=!Bitness! ====== | tee -a "%AKILOG%"
+	echo. | tee -a "%AKILOG%
 
-	echo Cleaning . . .
-	b2 --clean | tee "%AKILOG%"
+	echo **** Cleaning . . . | tee -a "%AKILOG%"
+	b2 --clean | tee -a "%AKILOG%"
+	echo. | tee -a "%AKILOG%
 
-	echo Building . . .
-	b2 install --prefix=%IntDir% --build-dir=%IntDir% --includedir=%OutDir%\..\include --libdir=%OutDir%\..\lib --toolset=msvc --no-cmake-config --layout=system variant=!Variant! address-model=!Bitness! | tee "%AKILOG%"
+	echo **** Building . . . | tee -a "%AKILOG%"
+	b2 install --prefix=%IntDir% --build-dir=%IntDir% --includedir=%OutDir%\..\include --libdir=%OutDir%\..\lib --toolset=msvc --no-cmake-config --layout=system variant=!Variant! address-model=!Bitness! | tee -a "%AKILOG%"
 	rmdir /s /q bin.v2 2>nul
 	del /f %OutDir%\%TargetFileName% 2>nul
+	echo. | tee -a "%AKILOG%
 
-	echo Finished
-
+	echo **** Finished. | tee -a "%AKILOG%"
 	popd
 )
 
@@ -49,15 +56,18 @@ if [%RunMode%] == [pre] (
 
 if [%RunMode%] == [post] (
 
-	echo **** Installing Boost headers ****
-	echo.
+	echo **** Installing Boost headers . . . | tee -a "%AKILOG%"
+	echo. | tee -a "%AKILOG%"
 
 	set "QDIR=%OutDir%\..\include\boost"
 	rem msgbox "CD=%CD%"
 	rem msgbox "QDIR=%QDIR%"
-	xcopy /d /y /r /s /i /f boost "!QDIR!" >nul
+	xcopy /d /y /r /s /i /f boost "!QDIR!" | tee -a "%AKILOG%"
 	if errorlevel 1 msgbox "XCOPY error %ERRORLEVEL%"
 	pushd "!QDIR!"
 	attrib +R /S
+	echo. | tee -a "%AKILOG%"
+
+	echo **** Finished. | tee -a "%AKILOG%"
 	popd
 )
