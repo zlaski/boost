78495660d5827aea0a564ed46031a8e699266e8d
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-12-11 22:35:06 -0800

    Update boost.akibld.bat

--- boost.akibld.bat
+++ boost.akibld.bat
@@ -1,14 +1,24 @@
-:: @echo off
+@echo off
 setlocal EnableExtensions EnableDelayedExpansion
-call "%AKIBLD%\build-event-env.bat" %~f0 %* 
+call "%AKIBLD%\akibld-setup.bat" %~f0 %* 
+
+set Bitness=unknown
+if /i [%Platform%] == ["Win32"] set Bitness=32
+if /i [%Platform%] == ["x86"] set Bitness=32
+if /i [%Platform%] == ["AMD64"] set Bitness=64
+if /i [%Platform%] == ["x64"] set Bitness=64
+
+set Variant=unknown
+if /i [%Configuration%] == ["Debug"] set Variant=debug
+if /i [%Configuration%] == ["Release"] set Variant=release
 
 :: boost.vcxproj pre-build 
 
 if [%RunMode%] == [clean] (
-	echo ******** CLEANING BOOST LIBRARY | tee -a "%AKILOG%"
+	echo ******** CLEANING BOOST LIBRARY
 	pushd "%ProjectDir%"
-	git clean -dxf -e .build | tee -a "%AKILOG%"
-	git submodule update --init --recursive | tee -a "%AKILOG%"
+	git clean -dxf -e .build
+	git submodule update --init --recursive
 	popd
 )
 
@@ -17,38 +27,29 @@ if [%RunMode%] == [pre] (
     rem msgbox "OutDir=%OutDir% TargetFileName=%TargetFileName%"
 	pushd "%ProjectDir%"
 
+	echo ====== variant=%Variant% address-model=%Bitness% ======
+
+	rem msgbox -k Variant: %Variant% or %Variant%
+
 	if not exist b2.exe (
-		echo ******** BUILDING B2 TOOL | tee -a "%AKILOG%"
-		bootstrap | tee -a "%AKILOG%"
-		echo. | tee -a "%AKILOG%"
+		echo ******** BUILDING B2 TOOL
+		bootstrap
 	)
 
-	echo ******** BUILDING BOOST LIBRARY | tee -a "%AKILOG%"
+	echo ******** BUILDING BOOST LIBRARY
 
-	set Bitness=64
-	if /i [%Platform%] == ["Win32"] set Bitness=32
-	if /i [%Platform%] == ["x86"] set Bitness=32
-	if /i [%Platform%] == ["AMD64"] set Bitness=64
-	if /i [%Platform%] == ["x64"] set Bitness=64
+	echo **** Cleaning . . .
+	b2 --clean
+	echo.
 
-	set Variant=release
-	if /i [%Configuration%] == [Debug] set Variant=debug
-	if /i [%Configuration%] == [Release] set Variant=release
+	echo ====== variant=%Variant% address-model=%Bitness% ======
+	echo.
 
-	echo ====== variant=!Variant! address-model=!Bitness! ====== | tee -a "%AKILOG%"
-	echo. | tee -a "%AKILOG%
+	echo **** Building . . .
+	b2 install --prefix=%IntDir% --build-dir=%IntDir% --includedir=%OutDir%\..\include --libdir=%OutDir%\..\lib --toolset=msvc --no-cmake-config --layout=system variant=%Variant% address-model=%Bitness%
+	echo.
 
-	echo **** Cleaning . . . | tee -a "%AKILOG%"
-	b2 --clean | tee -a "%AKILOG%"
-	echo. | tee -a "%AKILOG%
-
-	echo **** Building . . . | tee -a "%AKILOG%"
-	b2 install --prefix=%IntDir% --build-dir=%IntDir% --includedir=%OutDir%\..\include --libdir=%OutDir%\..\lib --toolset=msvc --no-cmake-config --layout=system variant=!Variant! address-model=!Bitness! | tee -a "%AKILOG%"
-	rmdir /s /q bin.v2 2>nul
-	del /f %OutDir%\%TargetFileName% 2>nul
-	echo. | tee -a "%AKILOG%
-
-	echo **** Finished. | tee -a "%AKILOG%"
+	echo **** Finished.
 	popd
 )
 
@@ -56,18 +57,18 @@ if [%RunMode%] == [pre] (
 
 if [%RunMode%] == [post] (
 
-	echo **** Installing Boost headers . . . | tee -a "%AKILOG%"
-	echo. | tee -a "%AKILOG%"
+	echo **** Installing Boost headers . . .
+	echo.
 
 	set "QDIR=%OutDir%\..\include\boost"
 	rem msgbox "CD=%CD%"
 	rem msgbox "QDIR=%QDIR%"
-	xcopy /d /y /r /s /i /f boost "!QDIR!" | tee -a "%AKILOG%"
+	xcopy /d /y /r /s /i /f boost "!QDIR!"
 	if errorlevel 1 msgbox "XCOPY error %ERRORLEVEL%"
 	pushd "!QDIR!"
 	attrib +R /S
-	echo. | tee -a "%AKILOG%"
+	echo.
 
-	echo **** Finished. | tee -a "%AKILOG%"
+	echo **** Finished.
 	popd
 )
